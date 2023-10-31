b609fef33b56adef5ccdcdd853deeaecd5e1a985
unknown <luisatavares217@gmail.com>
2023-10-31 16:11:11 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+  const img = document.querySelector("#profile img")
+  if(html.classList.contains('light')) {
+   img.setAttribute('src', './assets/foto op luz.jpg')
+  } else {
+   img.setAttribute('src', "./assets/foto op normal.jpg")
+  }
+
+}
